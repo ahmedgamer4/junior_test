f567da6f1754fb93ea1e625f159fa50b6595679f
ahmedgamer4 <aa10@pme.suezuni.edu.eg>
2023-04-18 02:32:34 +0200

    makeProduct(): Changed if else to a key value mapped array

--- backend/src/Factory.php
+++ backend/src/Factory.php
@@ -1,23 +1,28 @@
 <?php
+
 class Factory
 {
   private Database $database;
+  private array $product_types;
 
   public function __construct(Database $database)
   {
     $this->database = $database;
+    $this->product_types = [
+      'size' => DVDProductGateway::class,
+      'dimensions' => FurProductGateway::class,
+      'weigth' => BookProductGateway::class,
+    ];
   }
   public function makeProduct(array $data)
   {
-    if (!empty($data['size'])) {
-      return new DVDProductGateway($this->database);
+    foreach ($data as $key => $value) {
+      if (isset($this->product_types[$key]) && !empty($data[$key])) {
+        $productClass = $this->product_types[$key];
+        return new $productClass($this->database);
+      }
     }
-    if (!empty($data['weight'])) {
-      return new BookProductGateway($this->database);
-    }
-    if (!empty($data['dimensions'])) {
-      return new FurProductGateway($this->database);
-    }
+
     return new DVDProductGateway($this->database);
   }
 }
